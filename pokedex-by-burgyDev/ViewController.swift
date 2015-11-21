dfe1fd2f2e13936c5840df05f6e352c09dcf020a
김용재 <yongjaekim@yongui-macbook-pro.local>
2015-11-21 14:07:34 +0900

    Initial Commit

---
+++ pokedex-by-burgyDev/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-burgyDev
+//
+//  Created by Yong Jae Kim on 2015. 11. 21..
+//  Copyright © 2015년 Yong Jae Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
